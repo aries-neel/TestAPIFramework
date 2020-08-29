98168e4ed580d46992b01127cdd4b02126e5344e
aries-neel <innovativeneel@gmail.com>
2020-08-29 12:25:54 +0530

    adding mail dependency and Montoring mail /Test Config file

---
+++ src/test/java/com/InnovationCenter/Listeners/Rough.java
@@ -0,0 +1,14 @@
+package com.InnovationCenter.Listeners;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Rough {
+
+	public static void main(String[] args) throws UnknownHostException {
+		
+		System.out.println("http://"+InetAddress.getLocalHost().getHostAddress()+"D:\\RestAssuredAPI\\RestAssuredAPIFramework\\reports\\");
+		
+	}
+
+}
---
+++ src/test/java/com/InnovationCenter/Utility/TestConfig.java
@@ -0,0 +1,30 @@
+package com.InnovationCenter.Utility;
+
+public class TestConfig {
+	
+	public static String server="smtp.gmail.com";
+	public static String from = "neel190492@gmail.com";
+	public static String password = "Neel@1992";
+	public static String[] to ={"innovativeneel@gmail.com"};
+	public static String subject = "Extent Project Report";
+	
+	public static String messageBody ="TestMessage";
+	/*public static String attachmentPath="c:\\screenshot\\2017_10_3_14_49_9.jpg";
+	public static String attachmentName="error.jpg";*/
+	
+	
+	
+	//SQL DATABASE DETAILS	
+	public static String driver="net.sourceforge.jtds.jdbc.Driver"; 
+	public static String dbConnectionUrl="jdbc:jtds:sqlserver://192.101.44.22;DatabaseName=monitor_eval"; 
+	public static String dbUserName="sa"; 
+	public static String dbPassword="$ql$!!1"; 
+	
+	
+	//MYSQL DATABASE DETAILS
+	public static String mysqldriver="com.mysql.jdbc.Driver";
+	public static String mysqluserName = "root";
+	public static String mysqlpassword = "selenium";
+	public static String mysqlurl = "jdbc:mysql://localhost:3306/acs";
+
+}
